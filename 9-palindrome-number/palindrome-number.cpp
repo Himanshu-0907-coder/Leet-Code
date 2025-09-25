2517dfa7461d19580a3967ac6e8c00d114d93f29
Himanshu Maurya <hawkgaming603@gmail.com>
2025-09-25 11:17:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (91.89%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -3,12 +3,10 @@ public:
     bool isPalindrome(int x) {
         if(x<0) return false;
         long long rev = 0;
-        int n,r;
         int org = x;
         while(x>0){
-            n = x % 10;
-            rev = rev*10 + n;
-            x = x/10;
+            rev = rev*10 + (x%10);
+            x /= 10;
         }
         
         return (rev == org);
