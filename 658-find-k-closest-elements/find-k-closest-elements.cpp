90c1a7bbe233afab1656e5f77d2d545fd5103251
Himanshu Maurya <hawkgaming603@gmail.com>
2025-09-28 19:12:03 +0530

    Time: 0 ms (100.00%) | Memory: 35.7 MB (99.35%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -2,48 +2,76 @@ class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
         int n = arr.size();
-        vector<int> ans;
+        vector<int> ans(k);
+        if(x<arr[0]){
+            for(int i=0;i<k;i++){
+                ans[i] = arr[i];
+            }
+            return ans;
+        }
+        if(x>arr[n-1]){
+            int i = n-1;
+            int j = k-1;
+            while(j>=0){
+                ans[j] = arr[i];
+                i--;
+                j--; 
+            }
+            return ans;
+        }
 
-        int lo = 0, hi = n - 1, mid = -1;
-        while (lo <= hi) {
-            mid = lo + (hi - lo) / 2;
-            if (arr[mid] == x) {
+        int lo = 0;
+        int hi = n-1;
+        int t = 0;
+        bool flag = false;
+        int mid = -1;
+        while(lo<=hi){
+            mid = lo + (hi-lo)/2;
+            if(arr[mid] == x){
+                flag = true;
+                ans[t] = arr[mid];
+                t++;
                 break;
-            } else if (arr[mid] > x) hi = mid - 1;
+            }
+            else if(arr[mid] > x) hi = mid - 1;
             else lo = mid + 1;
         }
-
-        int left, right;
-        if (mid != -1 && arr[mid] == x) {
-            ans.push_back(arr[mid]);
-            left = mid - 1;
-            right = mid + 1;
-            k--; // one element already added
-        } else {
-            left = hi;
-            right = lo;
+        int lb = hi;
+        int ub = lo;
+        if(flag == true){
+            lb = mid-1;
+            ub = mid+1;
         }
+        while(t<k && lb>=0 && ub<=n-1){
+            int ld = abs(x-arr[lb]);
+            int rd = abs(x-arr[ub]);
+            if(ld <= rd){
+                ans[t] = arr[lb];
+                lb--;
+            } 
+            else {
+                ans[t] = arr[ub];
+                ub++;
+            }
 
-        while (k--) {
-            if (left < 0) {
-                ans.push_back(arr[right]);
-                right++;
-            } else if (right >= n) {
-                ans.insert(ans.begin(), arr[left]);
-                left--;
-            } else {
-                int ld = abs(x - arr[left]);
-                int rd = abs(x - arr[right]);
-                if (ld <= rd) {
-                    ans.insert(ans.begin(), arr[left]);
-                    left--;
-                } else {
-                    ans.push_back(arr[right]);
-                    right++;
-                }
+            t++;
+            
+        }
+        if(lb<0){
+            while(t<k){
+                ans[t] = arr[ub];
+                ub++;
+                t++;
             }
         }
-
+        if(ub>n-1){
+            while(t<k){
+                ans[t] = arr[lb];
+                lb--;
+                t++;
+            }
+        }
+        sort(ans.begin(),ans.end());
         return ans;
     }
 };
