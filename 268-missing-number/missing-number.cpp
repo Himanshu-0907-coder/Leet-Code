667bcc7886465340b99b9a76a7b5dc65450300ef
Himanshu Maurya <hawkgaming603@gmail.com>
2026-01-26 21:33:33 +0530

    Time: 9 ms (12.77%) | Memory: 21.6 MB (92.20%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -3,11 +3,20 @@ public:
     int missingNumber(vector<int>& nums) {
         sort(nums.begin(),nums.end());
 
-        for(int i=0;i<nums.size();i++){
-            if(i != nums[i]){
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != i){
                 return i;
             }
         }
-        return nums.size();
-    }
+        return nums.size(); 
+
+        // int lo = 0;
+        // int hi = nums.size()-1;
+        // while(lo<=hi){
+        //     int mid = lo+(hi-lo)/2;
+        //     if(nums[mid] == mid) lo = mid + 1;
+        //     else if(nums[mid]>mid) hi = mid - 1;
+        // }
+        // return lo;
+    }    
 };
