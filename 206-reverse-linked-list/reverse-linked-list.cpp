3d43f98c7bb19ccb753728649a5a49d319a91796
Himanshu Maurya <hawkgaming603@gmail.com>
2026-02-13 05:44:21 -0800

    Time: 0 ms (100.00%) | Memory: 13.5 MB (42.11%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,31 +1,43 @@
 class Solution {
 public:
-    ListNode* getNode(ListNode* head,int idx){
-        ListNode* temp = head;
-        for(int i=1;i<=idx;i++){
-            temp=temp->next;
-        }
-        return temp;
-    }
+    // ListNode* getNode(ListNode* head,int idx){
+    //     ListNode* temp = head;
+    //     for(int i=1;i<=idx;i++){
+    //         temp=temp->next;
+    //     }
+    //     return temp;
+    // }
     ListNode* reverseList(ListNode* head) {
-        int n = 0;
-        ListNode* temp = head;
-        while(temp){
+        // int n = 0;
+        // ListNode* temp = head;
+        // while(temp){
             
-            temp = temp->next;
-            n++;
+        //     temp = temp->next;
+        //     n++;
+        // }
+        // int i = 0;
+        // int j = n-1;
+        // while(i<j){
+        //     ListNode* left = getNode(head,i);
+        //     ListNode* right = getNode(head,j);
+        //     int t = left->val;
+        //     left->val = right->val;
+        //     right->val = t;
+        //     i++;
+        //     j--;
+        // }
+        // return head;
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* Next = NULL;
+        while(curr){
+            Next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = Next;
         }
-        int i = 0;
-        int j = n-1;
-        while(i<j){
-            ListNode* left = getNode(head,i);
-            ListNode* right = getNode(head,j);
-            int t = left->val;
-            left->val = right->val;
-            right->val = t;
-            i++;
-            j--;
-        }
-        return head;
+        return prev;
+
     }
 };
