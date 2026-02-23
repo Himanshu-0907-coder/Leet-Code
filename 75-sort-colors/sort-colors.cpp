5342468391d384338db9f3314ac1d394c042eb88
Himanshu Maurya <hawkgaming603@gmail.com>
2026-02-23 10:35:08 -0800

    Time: 0 ms (100.00%) | Memory: 11.7 MB (47.88%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,7 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        
+       int n = nums.size();
+       int noz = 0;
+       int noo = 0;
+       int notw = 0;
+       // counting zeros and ones two's
+       for(int i=0;i<n;i++){
+            if(nums[i] == 0){
+                noz++;
+            }
+            else if(nums[i] == 1){
+                noo++;
+            }
+            else{
+                notw++;
+            }
+        }
+        // filling ones,two and ones
+        for(int i=0;i<n;i++){
+            if(i < noz){
+                nums[i] = 0;
+            }
+            else if(i < (noz+noo)){
+                nums[i] = 1;
+            }
+            else{
+                nums[i] = 2;
+            }
+        }
     }
 };
