51f998186b71e3f37c0651afc6bff3f5b4a26c37
Himanshu Maurya <hawkgaming603@gmail.com>
2026-02-24 07:41:43 -0800

    Time: 0 ms (100.00%) | Memory: 28.2 MB (27.43%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        if(nums.size() == 1) return nums[0];
+        // if(nums.size() == 1) return nums[0];
         sort(nums.begin(),nums.end());
-        int count = 0;
-        int maxCount = INT_MIN;
-        int ans;
-        for(int i=1;i<nums.size();i++){
-            if(nums[i] == nums[i-1]){
-                count++;
-                if(maxCount < count){
-                    ans = nums[i];
-                    maxCount = max(maxCount,count);
-                }
-            }
-            else{
-                count = 0;
-            }
-        }
-        return ans;
+        // int count = 0;
+        // int maxCount = INT_MIN;
+        // int ans;
+        // for(int i=1;i<nums.size();i++){
+        //     if(nums[i] == nums[i-1]){
+        //         count++;
+        //         if(maxCount < count){
+        //             ans = nums[i];
+        //             maxCount = max(maxCount,count);
+        //         }
+        //     }
+        //     else{
+        //         count = 0;
+        //     }
+        // }
+        // return ans;
+        return nums[(nums.size())/2];
     }
 };
