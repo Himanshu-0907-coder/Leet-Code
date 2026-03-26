8a18225d76b88da07ca1fdbf99240b8f71ea06ed
Himanshu Maurya <hawkgaming603@gmail.com>
2026-03-26 12:57:21 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (10.31%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-
-        if(s == t){
-            return true;
+        if(s.length() != t.length()) return false;
+        unordered_map<char,int> map1;
+        unordered_map<char,int> map2;
+        //insertinf element in map1 of string s
+        for(int i=0;i<s.length();i++){
+            map1[s[i]]++; // charcater insert bhi kar rhe aur length bhi badha rhe;
         }
-        return false;
+        for(int i=0;i<s.length();i++){
+            map2[t[i]]++; // insert bhi kar rhe aur length bhi badha rhe;
+        }
+        //checking for anagram
+        for(auto ele : map1){
+            char ch1 = ele.first;
+            int freq = ele.second;
+            if(map2.find(ch1) != map2.end()){
+                int freq2 = map2[ch1];
+                if(freq != freq2) return false;
+            }
+            else return false;
+        }
+        return true;
     }
 };
