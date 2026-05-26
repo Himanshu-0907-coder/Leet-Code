06c87e41d26ad718e5aeca2946cf34c1880c1778
Himanshu Maurya <hawkgaming603@gmail.com>
2026-05-26 17:49:47 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (91.36%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -1,38 +1,25 @@
 class Solution {
 public:
-    void reversePart(vector<int> &nums,int i,int j){
+    void reverse(vector<int> &nums,int i,int j){
         while(i<j){
-            int t = nums[i];
+            int temp = nums[i];
             nums[i] = nums[j];
-            nums[j] = t;
+            nums[j] = temp;
             i++;
             j--;
         }
-    } 
+    }
     void rotate(vector<int>& nums, int k) {
-        // int n = nums.size();
-        // k=k%n;
-        // int idx = n-k-1;
-        // vector<int> v;
-        // for(int i=idx+1;i<n;i++){
-        //     v.push_back(nums[i]);
-        // }
-        // for(int i=0;i<=idx;i++){
-        //     v.push_back(nums[i]);
-        // }
-        // nums = v;
         int n = nums.size();
         k = k%n;
-        //whole array reverse
-        reversePart(nums,0,n-1);
 
-        //reverse ing th k element
-        reversePart(nums,0,k-1);
+        //Rev whole array
+        reverse(nums,0,n-1);
 
-        //reversing the n-k element
-        reversePart(nums,k,n-1);
-        
-        
+        //rev k element
+        reverse(nums,0,k-1);
 
+        //reversing after k element
+        reverse(nums,k,n-1);
     }
 };
