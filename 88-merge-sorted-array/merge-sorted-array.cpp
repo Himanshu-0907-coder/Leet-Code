aec5752607ac70a204491ec0d62b00e872046013
Himanshu Maurya <hawkgaming603@gmail.com>
2026-05-28 19:12:53 +0530

    Time: 3 ms (2.36%) | Memory: 12.6 MB (8.13%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> v(m+n);
-        int i = m-1;
-        int j = n-1;
-        int k = m+n-1;
-        while(i >= 0 && j >= 0){
-            if(nums1[i]> nums2[j]){
-                v[k] = nums1[i];
-                i--;
+        if(m == 1 && n == 0) nums1;
+        
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        vector<int> ans(m+n);
+
+        while(i<m && j<n){
+            if(nums1[i] <= nums2[j]){
+                ans[k] = nums1[i];
+                k++;
+                i++;
             }
             else{
-                v[k] = nums2[j];
-                j--;
+                ans[k] = nums2[j];
+                k++;
+                j++;
             }
-            k--;
         }
-        while(i >= 0){
-            v[k] = nums1[i];
-            i--;
-            k--;
+        while(i<m){
+            ans[k] = nums1[i];
+            i++;
+            k++;
         }
-        while(j >= 0){
-            v[k] = nums2[j];
-            j--;
-            k--;
+        while(j<n){
+            ans[k] = nums2[j];
+            j++;
+            k++;
         }
-        nums1 = v;
+        nums1 = ans;
     }
 };
