1a73de66db2023a12813b427bcf97149a2aa0f8a
Himanshu Maurya <hawkgaming603@gmail.com>
2026-08-17 10:07:39 +0530

    Time: 3 ms (37.16%) | Memory: 42.3 MB (8.45%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int n =nums.size();
-
-        int prod = 1;
-        vector<int>a1;
+        int n = nums.size();
+        vector<int> pre(n);
+        vector<int> suf(n);
+        vector<int> ans(n);
+        //Prefix
+        int p = nums[0];
+        pre[0] = 1;
+        for(int i=1;i<n;i++){
+            pre[i] = p;
+            p *= nums[i];
+        }
+        //Suffix
+        p = nums[n-1];
+        suf[n-1] = 1;
+        for(int i=n-2;i>=0;i--){
+            suf[i] = p;
+            p *= nums[i];
+        } 
+        //Final ans 
         for(int i=0;i<n;i++){
-            a1.push_back(prod);
-            prod = prod * nums[i];
+            ans[i] = pre[i]*suf[i];
         }
-
-        prod = 1;
-        vector<int>a2;
-        for(int i=n-1;i>=0;i--){
-            a2.push_back(prod);
-            prod = prod * nums[i];
-        }
-
-        reverse(a1.begin(),a1.end());
-        vector<int>ans(n);
-        for(int i =0;i<n;i++){
-            ans[i] = a1[i] * a2[i];
-        }
-        
-        reverse(ans.begin(),ans.end());
         return ans;
     }
 };
